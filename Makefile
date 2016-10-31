af8573952683d87727b9dfdfd44dc03cce8c5449
Devin Lehmacher <lehmacdj@gmail.com>
2016-10-31 12:23:10 -0400

    Don't use ppx_inline_test

--- Makefile
+++ Makefile
@@ -19,12 +19,10 @@ mli:
 	@mv *.cmi _build/
 
 tests:
-	$(CC) -Is .,test -package ounit $(PACKAGES) edCommand_test.byte
+	$(CC) -Is .,test -package ounit $(PACKAGES) edCommand_test.byte >/dev/null 2>&1
 
 .PHONY: test
 
-TEST_ARGS=inline-test-runner ocaml-ed
-
 test: tests
 	@echo Testing edCommand...
-	@./edCommand_test.byte $(TEST_ARGS)
+	@./edCommand_test.byte
--- test/edCommand_test.ml
+++ test/edCommand_test.ml
@@ -1,6 +1,15 @@
 open EdCommand.Types
 open OUnit2
 
-let edit = Completed Edit File "hello"
+let edit = Edit (File "hello")
 
-let%test "edit" = edit = (Completed Edit File "hello")
+let test1 test_ctxt = assert_equal edit (Edit (File "hello"))
+let test2 test_ctxt = assert_equal 2 (1 + 1)
+
+let suite =
+  "suite" >::: [
+    "test1" >:: test1;
+    "test2" >:: test2;
+  ]
+
+let () = run_test_tt_main suite
